aea59b13be090c931b6a50415d4caff70fe1acb7
hmt <dev@hmt.im>
2020-12-09 13:11:17 +0100

    Klassenweise

--- Webuntis-Fotos.svelte
+++ Webuntis-Fotos.svelte
@@ -1,25 +1,33 @@
 <script>
   const mysql = R("mysql");
-  const fs = R('fs')
-  export let knexConfig
+  const fs = R("fs");
+  const _ = R("lodash");
+  export let knexConfig;
+  let base = "/tmp/fotos";
   const mysql_connection = mysql.createConnection(knexConfig.connection);
   mysql_connection.connect();
   mysql_connection.query(
     `
-  SELECT f.Foto, s.ID
+  SELECT f.Foto, s.ID, SUBSTRING(s.Klasse, 1, 4) AS Klasse
   FROM schuelerfotos AS f
   JOIN schueler AS s ON (f.Schueler_ID = s.ID)
   WHERE s.Status = 2 AND s.Geloescht = "-" AND s.Gesperrt = "-"
   `,
     (e, res) => {
       console.log(e, res.length);
-      res.forEach(f => {
-        const data = new Uint8Array(Buffer.from(f.Foto, 'binary'));
-        fs.writeFile(`/tmp/fotos/${f.ID}.jpg`, data, (err) => {
-          if (err) throw err;
+      Object.entries(_.groupBy(res, "Klasse")).forEach((k) => {
+        fs.mkdirSync(`${base}/${k[0]}`);
+        k[1].forEach((f) => {
+          const data = new Uint8Array(Buffer.from(f.Foto, "binary"));
+          fs.writeFile(`${base}/${k[0]}/${f.ID}.jpg`, data, (err) => {
+            if (err) throw err;
+          });
         });
       });
     }
   );
 </script>
-Fotos werden unter <b>/tmp/fotos</b> abgelegt. Verzeichnis muss vorhanden sein.
+
+Fotos werden unter
+<b>{base}</b>
+abgelegt. Verzeichnis muss vorhanden sein.
