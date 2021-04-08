c8a4b95ef5349940e40fff007d77d8eb871c80de
hmt <dev@hmt.im>
2021-04-08 18:55:58 +0200

    lehrer paedml

---
+++ CSV Import-PaedML-Lehrer copy.svelte
@@ -0,0 +1,21 @@
+<script>
+  const mysql = R("mysql");
+  export let knexConfig
+  let gruppe = []
+  const mysql_connection = mysql.createConnection(knexConfig.connection);
+  mysql_connection.connect();
+  mysql_connection.query(
+    ` SELECT Nachname, Vorname, Email, Kuerzel FROM k_lehrer WHERE Sichtbar="+";`,
+    (e, res) => {
+      console.log(e);
+      gruppe = res;
+    }
+  );
+</script>
+
+<pre>
+{#each gruppe as s}
+{s.Kuerzel},{s.Nachname},{s.Vorname},{s.Email}<br>
+{:else}kommt...
+{/each}
+</pre>
