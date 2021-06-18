2ea38caac7f42883028c9c12ddca49fcc6f679d9
hmt <dev@hmt.im>
2021-06-18 08:35:40 +0200

    Beispiel Tabelle

---
+++ Beispiel.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let schueler, jahr, abschnitt, klasse;
+</script>
+
+<h2>Klassenliste der {klasse.Klasse} für {jahr}, {abschnitt}. Halbjahr</h2>
+<table border="1">
+  {#each schueler as s}
+    <tr>
+      <td>{s.Name}</td><td>{s.Vorname}</td><td>{s.Telefon}</td><td
+        >{s.EMail || ""}</td
+      >
+    </tr>
+  {/each}
+</table>
