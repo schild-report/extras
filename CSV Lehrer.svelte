d248582f8a4e51d5d046797d74efe25bc17d97ad
hmt <dev@hmt.im>
2022-09-01 11:25:13 +0200

    aktuelles

---
+++ CSV Lehrer.svelte
@@ -0,0 +1,22 @@
+<script>
+  const mysql = R("mysql");
+  export let knexConfig
+  let gruppe = []
+  const mysql_connection = mysql.createConnection(knexConfig.connection);
+  mysql_connection.connect();
+  mysql_connection.query(
+    ` SELECT ID, Nachname, Vorname, Kuerzel FROM k_lehrer WHERE Sichtbar="+";`,
+    (e, res) => {
+      console.log(e);
+      gruppe = res;
+    }
+  );
+</script>
+
+<pre>
+id,name,vorname,Kuerzel
+{#each gruppe as s}
+{s.ID},{s.Nachname},{s.Vorname},{s.Kuerzel}<br>
+{:else}kommt...
+{/each}
+</pre>
