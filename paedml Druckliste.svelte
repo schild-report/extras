1508a73d0410077a5e41fc980cd1f73862d2bcac
hmt <dev@hmt.im>
2023-06-13 09:23:04 +0200

    druckliste

--- paedml Druckliste.svelte
+++ paedml Druckliste.svelte
@@ -2,7 +2,7 @@
 Hallo {schueler[0].Vorname},
 
 Ihr Benutzername: b{schueler[0].ID}
-Ihr Passwort: {h(schueler[0].ID)}
+Ihr Passwort: {h(schueler[0])}
 
 Verwenden Sie für Ihren persönlichen Zugang {privat.meinbk}
 
@@ -12,8 +12,9 @@ Grüße
   import Hashids from 'hashids'
   export let schueler, privat
   if (!privat.paedml_salt) throw 'Kein Salt'
+  const prefix = (s) => String(s.SchulnrEigner) === String(privat.schulnummer) ? "b":"k"
   const hashids = new Hashids(privat.paedml_salt, 8, 'abcdefghkmnpqrstuvwxyz23456789')
-  const h = (id) => hashids.encode(id)
+  const h = (s) => hashids.encode((prefix(s) === 'b' ? 1:2 )+ s.ID)
 </script>
 <style>
   .big {
