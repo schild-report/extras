ab0a0e889ffc0c6ac354b9ac4c3d8abc60db34bf
hmt <dev@hmt.im>
2024-08-21 07:48:34 +0200

    Revert "updates"
    This reverts commit d2a673268db4300d265e37253178ce226fa84d8c.

--- paedml Druckliste alle.svelte
+++ paedml Druckliste alle.svelte
@@ -3,30 +3,28 @@
     {#each _.chunk(schueler, 6) as slice}
       <div class="page" orientation="portrait" size="A4" style="font-family: sans; font-size: .98rem">
         <div class="grid">
-          <div style="display: flex; justify-content: space-between; padding: 8px 8px 0px 8px;">
-            <div><b>{klasse}</b></div> 
-            <div>{datum(Date.now())}</div>
+          <div style="display: flex; justify-content: space-between;">
+            <div><b style="padding: 5px 0 0 5px">{klasse}</b></div> 
+            <div style="padding: 5px 5px 0 0 ">{datum(Date.now())}</div>
           </div>
           {#each slice as s}
             <div>
-              <table style="width: 90%; margin-bottom: 8px; margin-left: 25px">
+              <table style="width: 80%; margin-bottom: 8px; margin-left: 30px">
                 <tr>
-                  <td colspan="2"><b>{s.Name}, {s.Vorname}</b></td>
+                  <td style="width: 60%"><b>{s.Name}, {s.Vorname}</b></td>
+                  <td>Ihr Benutzername: <code> {s.prefix}{s.ID}</code></td>
                 </tr>
                 <tr>
-                  <td width="25%">Ihr Benutzername: <code> {s.username}</code></td>
-                  <td>Ihr Passwort: <code>{h(s.hash)}</code></td>
-                </tr>
-                <tr>
-                  <td colspan="2">Teams: <code>{s.slug}@{privat.domain}</code></td>
+                  <td></td>
+                  <td>Ihr Passwort: <code>{h((s.prefix === 'b' ? 1:2) + s.ID)}</code></td>
                 </tr>
               </table>
-            <div style="padding: 0 10px 0 30px;">
-              <b>Zugangsdaten zum Pädagogischen Netz des {privat.schulname} für das Schuljahr 2024/25</b>
+            <div style="padding: 0 30px 0 30px;">
+              <b>Zugangsdaten zum Pädagogischen Netz des {privat.schulname} für das Schuljahr 2023/24</b>
               <br>Mit der Verwendung der Zugangsdaten bestätigen Sie Ihre Zustimmung zur Nutzungsordnung für das
               Pädagogische Netz.
               <br>Unter <b>https://{privat.meinbk}</b> können Sie einen persönlichen WLAN-Zugang anfordern.
-              <br>Heben Sie diesen Zettel das ganze Schuljahr über gut auf und <b>machen Sie zur Sicherheit ein Foto davon.</b>
+              <br>Heben Sie diesen Zettel das ganze Schuljahr über gut auf und machen Sie zur Sicherheit ein Foto davon.
             </div>
             </div>
             <hr>
@@ -43,18 +41,18 @@
   .grid {
     display: grid;
     grid-template-columns: repeat(1, 1fr);
-    grid-gap: 8px;
+    grid-gap: 20px;
 }
 </style>
 
 <script>
-  import Hashids from 'hashids';
+  import Hashids from 'hashids'
   import { datum, updater } from './helfer';
-  const mysql = R('mysql');
-  const _ = R('lodash');
-  export let knexConfig, privat;
-  let regel = [], foerder = [], gruppe = [];
-  if (!privat.paedml_salt || !privat.schulname || !privat.meinbk) throw "Daten privat fehlen"
+  const mysql = R('mysql')
+  const _ = R('lodash')
+  export let knexConfig, privat
+  let gruppe = [], regel, foerder
+  if (!privat.schulnummer || !privat.paedml_salt || !privat.schulname || !privat.meinbk) throw "Daten privat fehlen"
   const hashids = new Hashids(privat.paedml_salt, 8, 'abcdefghkmnpqrstuvwxyz23456789')
   const h = (id) => hashids.encode(id)
   const mysql_connection = mysql.createConnection(knexConfig.connection);
@@ -62,14 +60,14 @@
   const mysql_connection2 = mysql.createConnection(knexConfig.connection);
   mysql_connection.connect();
   mysql_connection2.connect();
-  const query = `SELECT ID, Name, Vorname, Klasse, Geburtsdatum, GU_ID
+  const query = `SELECT ID, Name, Vorname, Klasse, Geburtsdatum, 
+                    SchulnrEigner as Schulnummer
                 FROM schueler
-                WHERE Status = 2 AND Geloescht = "-" AND Gesperrt = "-"
+                WHERE Status = 2
+                  AND Geloescht = "-"
+                  AND Gesperrt = "-"
                 ORDER BY ASDSchulform, Klasse, Name ASC`
-  mysql_connection.query(query, async (e,res)=> e ? console.log(e, "reg"): (regel = res))
-  mysql_connection2.query(query, async (e,res)=> e ? console.log(e, "förder"): (foerder = res))
-  $: {
-    if (regel.length && foerder.length)
-      gruppe = updater(regel.concat(foerder));
-    };
+  mysql_connection.query(query, async (e,res)=> e ? console.log(e, "reg"): (regel = updater(res, privat)))
+  mysql_connection2.query(query, async (e,res)=> e ? console.log(e, "förder"): (foerder = updater(res, privat)))
+  $: if (regel && foerder) gruppe = gruppe.concat(regel, foerder)
 </script>
